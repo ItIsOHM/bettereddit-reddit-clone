1af522e9060373ef2fa73e9f9767922fb1935417
Rhythm Garg <rhythmgarg05@gmail.com>
2024-02-09 01:30:51 +0530

    change case on `@/components/ui/ui/toast`

--- src/components/ui/ui/Toaster.tsx
+++ src/components/ui/ui/Toaster.tsx
@@ -7,7 +7,7 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/ui/toast"
+} from "@/components/ui/ui/Toast"
 import { useToast } from "@/hooks/use-toast"
 
 export function Toaster() {
